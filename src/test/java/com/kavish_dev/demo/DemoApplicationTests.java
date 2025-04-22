162b631d078d29d9d18115485c406dc00c52fd5b
Kavish Rizvi <kavish.rizvi.dev@gmail.com>
2025-04-23 02:29:11 +0530

    Initial Commit

---
+++ src/test/java/com/kavish_dev/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavish_dev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
